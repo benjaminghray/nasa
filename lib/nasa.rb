6180f7f39ca22caea303482abac1d21b60532559
Benjamin Anderson <benjaminghray@gmail.com>
2020-12-06 22:33:34 -0600

    new

--- lib/nasa.rb
+++ lib/nasa.rb
@@ -4,7 +4,10 @@ module Nasa
   class Cli
     def greeting
       puts "Hey, Welcome to the Nasa World Event gem. Would you like to view a list of natural events/disters and get information on them? "
-      input = gets.chomp.downcase #gets string and makes it lowercase
+      puts "(type 'yes','no', or 'exit')"
+      puts ""
+      input = gets.chomp.downcase 
+      while input != "exit" do
       if input == 'yes'
         Nasa::Categories.display_categories
         select_categorie
@@ -17,9 +20,7 @@ module Nasa
     def select_categorie
       puts "please enter corresponding number under #{Nasa::Categories.titles.length+1}"
       input = gets.chomp
-      if input == 'exit'
         puts 'Thanks for using my application!'
-          exit
       else
         input = input.to_i
         if input == 0 || !input.between?(1,Nasa::Categories.titles.length)
